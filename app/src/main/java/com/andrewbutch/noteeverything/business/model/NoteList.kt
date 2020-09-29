c97d155fb6d2dbdd7a56d79df83a887ab12a81fe
Andrew Butch <andre_zip@list.ru>
2020-09-29 21:26:13 +0300

    Added business models

---
+++ app/src/main/java/com/andrewbutch/noteeverything/business/model/NoteList.kt
@@ -0,0 +1,35 @@
+package com.andrewbutch.noteeverything.business.model
+
+
+data class NoteList(
+    val id: String,
+    val title: String,
+    val color: String,
+    val created_at: String,
+    val updated_at: String,
+    val notes: List<Note>
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as NoteList
+
+        if (id != other.id) return false
+        if (title != other.title) return false
+        if (color != other.color) return false
+        if (created_at != other.created_at) return false
+        if (notes != other.notes) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + title.hashCode()
+        result = 31 * result + color.hashCode()
+        result = 31 * result + created_at.hashCode()
+        result = 31 * result + notes.hashCode()
+        return result
+    }
+}
