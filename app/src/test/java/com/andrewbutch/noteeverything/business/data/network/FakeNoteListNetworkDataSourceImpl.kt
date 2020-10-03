f4c917d98ad629acf2f4330e53d0b378d3a92d83
Andrew Butch <andre_zip@list.ru>
2020-10-03 16:21:32 +0300

    Added fake NOTE LIST network data source implementation

---
+++ app/src/test/java/com/andrewbutch/noteeverything/business/data/network/FakeNoteListNetworkDataSourceImpl.kt
@@ -0,0 +1,24 @@
+package com.andrewbutch.noteeverything.business.data.network
+
+import com.andrewbutch.noteeverything.business.data.network.abstraction.NoteListNetworkDataSource
+import com.andrewbutch.noteeverything.business.domain.model.NoteList
+
+class FakeNoteListNetworkDataSourceImpl(
+    private val data: HashMap<String, NoteList>
+) : NoteListNetworkDataSource {
+    override suspend fun insertOrUpdateNoteList(noteList: NoteList) {
+        data[noteList.id] = noteList
+    }
+
+    override suspend fun deleteNoteList(id: String) {
+        data.remove(id)
+    }
+
+    override suspend fun deleteAllNotesLists() {
+        data.clear()
+    }
+
+    override suspend fun searchNoteList(noteList: NoteList): NoteList? = data[noteList.id]
+
+    override suspend fun getAllNotes(): List<NoteList> = ArrayList(data.values)
+}
