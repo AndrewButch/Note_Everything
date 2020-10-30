b4bdce7b74d06038f89f5aa5eb36a5d031ee67a3
Andrew Butch <andre_zip@list.ru>
2020-10-30 20:26:51 +0300

    Added update note use case and view state

---
+++ app/src/main/java/com/andrewbutch/noteeverything/business/interactors/notedetail/UpdateNote.kt
@@ -0,0 +1,80 @@
+package com.andrewbutch.noteeverything.business.interactors.notedetail
+
+import com.andrewbutch.noteeverything.business.data.cache.CacheResultHandler
+import com.andrewbutch.noteeverything.business.data.cache.abstraction.NoteCacheDataSource
+import com.andrewbutch.noteeverything.business.data.network.abstraction.NoteNetworkDataSource
+import com.andrewbutch.noteeverything.business.data.util.safeCacheCall
+import com.andrewbutch.noteeverything.business.data.util.safeNetworkCall
+import com.andrewbutch.noteeverything.business.domain.model.Note
+import com.andrewbutch.noteeverything.business.domain.state.*
+import com.andrewbutch.noteeverything.framework.ui.notedetail.state.NoteDetailViewState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class UpdateNote(
+    private val noteCacheDataSource: NoteCacheDataSource,
+    private val noteNetworkDataSource: NoteNetworkDataSource
+) {
+
+    fun updateNote(
+        note: Note,
+        stateEvent: StateEvent
+    ): Flow<DataState<NoteDetailViewState>?> = flow {
+        val cacheResult = safeCacheCall(Dispatchers.IO) {
+            noteCacheDataSource.updateNote(
+                id = note.id,
+                newTitle = note.title,
+                newColor = note.color,
+                timestamp = null, // will be generated
+                completed = note.completed
+            )
+        }
+
+        val cacheResponse = object : CacheResultHandler<NoteDetailViewState, Int>(
+            result = cacheResult,
+            stateEvent = stateEvent
+        ) {
+            override suspend fun handleSuccess(resultValue: Int): DataState<NoteDetailViewState>? {
+                return if (resultValue > 0) {
+                    DataState(
+                        stateMessage = StateMessage(
+                            message = UPDATE_NOTE_SUCCESS,
+                            uiComponentType = UIComponentType.Toast,
+                            messageType = MessageType.Success
+                        ),
+                        data = null,
+                        stateEvent = stateEvent
+                    )
+                } else {
+                    DataState(
+                        stateMessage = StateMessage(
+                            message = UPDATE_NOTE_FAILURE,
+                            uiComponentType = UIComponentType.Toast,
+                            messageType = MessageType.Error
+                        ),
+                        data = null,
+                        stateEvent = stateEvent
+                    )
+                }
+            }
+        }.getResult()
+
+        emit(cacheResponse)
+
+        updateNetwork(cacheResponse?.stateMessage?.message, note)
+    }
+
+    private suspend fun updateNetwork(responseMsg: String?, note: Note) {
+        if (responseMsg == UPDATE_NOTE_SUCCESS) {
+            safeNetworkCall(Dispatchers.IO) {
+                noteNetworkDataSource.insertOrUpdateNote(note)
+            }
+        }
+    }
+
+    companion object {
+        const val UPDATE_NOTE_SUCCESS = "Successfully updated note"
+        const val UPDATE_NOTE_FAILURE = "Failed to update note"
+    }
+}
---
+++ app/src/main/java/com/andrewbutch/noteeverything/framework/ui/notedetail/state/NoteDetailViewState.kt
@@ -0,0 +1,11 @@
+package com.andrewbutch.noteeverything.framework.ui.notedetail.state
+
+import android.os.Parcelable
+import com.andrewbutch.noteeverything.business.domain.model.Note
+import com.andrewbutch.noteeverything.business.domain.state.ViewState
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class NoteDetailViewState(
+    private var note: Note? = null
+) : Parcelable, ViewState
