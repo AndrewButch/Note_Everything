8afc6383b5ac313938c28831911d8c0a5d66ebff
Andrew Butch <andre_zip@list.ru>
2020-11-14 14:20:51 +0300

    SplashFragment animation

--- app/src/main/java/com/andrewbutch/noteeverything/framework/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/andrewbutch/noteeverything/framework/ui/splash/SplashFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import com.andrewbutch.noteeverything.R
 import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.splash_fragment.*
 import javax.inject.Inject
+
 
 class SplashFragment : DaggerFragment() {
     @Inject
@@ -25,6 +29,7 @@ class SplashFragment : DaggerFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        startSyncAnimation()
         viewModel =
             ViewModelProvider(viewModelStore, providerFactory).get(SplashViewModel::class.java)
         viewModel.syncHasBeenExecuted().observe(viewLifecycleOwner) { syncCompleted ->
@@ -34,6 +39,13 @@ class SplashFragment : DaggerFragment() {
                     .navigate(R.id.action_splashFragment_to_notesFragment)
             }
         }
+    }
+
+    private fun startSyncAnimation() {
+        val animation: Animation = AnimationUtils.loadAnimation(
+            context, R.anim.rotate
+        )
+        syncImage.startAnimation(animation)
     }
 
 }
