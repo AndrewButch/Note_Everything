43fbc665c9d358cb4d3817145ba80ca777d80965
Andrew Butch <andre_zip@list.ru>
2020-12-05 18:24:49 +0300

    Include Timber for logging

--- app/src/main/java/com/andrewbutch/noteeverything/framework/BaseApplication.kt
+++ app/src/main/java/com/andrewbutch/noteeverything/framework/BaseApplication.kt
@@ -1,10 +1,12 @@
 package com.andrewbutch.noteeverything.framework
 
+import com.andrewbutch.noteeverything.BuildConfig
 import com.andrewbutch.noteeverything.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
+import timber.log.Timber
 
 @FlowPreview
 @ExperimentalCoroutinesApi
@@ -12,6 +14,13 @@ open class BaseApplication : DaggerApplication() {
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication>? {
         return DaggerAppComponent.builder().application(this).build()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        if(BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 
 
